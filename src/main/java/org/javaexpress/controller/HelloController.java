beb040fc32c40e6ff4dbb04567bebfc055c96e59
javaexpresschannel <javaexpreschannel@gmail.com>
2022-05-02 10:38:39 +0530

    initial commit

---
+++ src/main/java/org/javaexpress/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.javaexpress.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String getHello() {
+		return "Welcome to Spring Boot Course";
+	}
+}
